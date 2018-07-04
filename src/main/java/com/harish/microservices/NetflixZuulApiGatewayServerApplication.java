de380f9559a433e56e829d1154dbb0b69f6717e6
hparameswaran <harishp@hotmail.com>
2018-07-04 18:02:51 -0400

    First commit

---
+++ src/main/java/com/harish/microservices/NetflixZuulApiGatewayServerApplication.java
@@ -0,0 +1,25 @@
+package com.harish.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+import org.springframework.cloud.sleuth.*;
+
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NetflixZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSampler() {
+		
+		return new Sampler.
+	}
+}
